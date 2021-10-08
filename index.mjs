d5a54552d811cdfb3366e1d6aa52c0bdf35df322
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-10-08 15:49:45 -0300

    Finished get comments end point

--- index.mjs
+++ index.mjs
@@ -13,6 +13,19 @@ const posts = [{
     commentCount: 2
   }];
 
+
+const postsComments = [{
+    id: 1,
+    postId: 1,
+    author: 'João',
+    content: 'Muito bom esse post! Tá de parabéns'
+  }, {
+    id: 2,
+    postId: 1,
+    author: 'Maria',
+    content: 'Como faz pra dar palmas?'
+  }]
+
 app.get("/posts", (req, res) => {
     res.send(posts);
 })
@@ -21,5 +34,11 @@ app.get("/posts/:id", (req, res) => {
     const id = parseInt(req.params.id);
     res.send(posts.find((post)=>post.id === id));
 })
+
+app.get("/posts/:id/comments", (req, res) => {
+    const id = parseInt(req.params.id);
+    res.send(postsComments.filter((comment)=>comment.postId === id));
+})
+
 
 app.listen(4000);
