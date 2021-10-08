6f765c86338e94ec69a7903e050e2b975db1bde0
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-10-08 15:12:08 -0300

    Added posts end point

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import cors from 'cors'
+import express from 'express'
+
+const app = express();
+app.use(cors());
+
+const posts = [{
+    id: 1,
+    title: 'Hello World',
+    coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
+    contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
+    content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
+    commentCount: 2
+  }];
+
+app.get("/posts", (req, res) => {
+    res.send(posts);
+})
+
+app.listen(4000);
